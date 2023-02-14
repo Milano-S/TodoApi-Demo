206114a1bf31fa98ba1142e68340604c31e77cc9
Milano <milanoscharnick340@gmail.com>
2023-02-14 12:20:22 +0200

    Initial Commit

---
+++ TodoApi/TodoApi/Interfaces/ITodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using TodoApi.Models;
+
+namespace TodoApi.Interfaces
+{
+    public interface ITodoRepository
+    {
+        bool DoesItemExist(string id);
+        IEnumerable<TodoItem> All { get; }
+        TodoItem Find(string id);
+        void Insert(TodoItem item);
+        void Update(TodoItem item);
+        void Delete(string id);
+    }
+}
